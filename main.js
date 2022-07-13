f8d5621a4de53bc0be972f00e661e47946488634
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-13 16:37:22 +0200

    bonus

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ const app = new Vue({
     el: '#root',
     data: {
         mails: [],
-        generationDone: false,
     },
     created() {
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
-                    const result = response.data.response;
-                    this.mails.push(result);
-                })
+                    this.mails.push(response.data.response);
+                });
         }
-        this.generationDone = true;
     }
 });
